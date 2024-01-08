4a89e1aae00876c14576b9436831577cd66c3afd
cwp0 <wenpengchen@njust.edu.cn>
2024-01-08 17:04:38 +0800

    update code.

--- Python/test.py
+++ Python/test.py
@@ -7,12 +7,25 @@
 # @Version: 1.0
 
 
+#!/usr/bin/python3
 
-tinydict = {'Name': 'Runoob', 'Age': 7, 'Class': 'First'}
+import _thread
+import time
 
-del tinydict['Name']  # 删除键 'Name'
-tinydict.clear()  # 清空字典
-del tinydict  # 删除字典
+# 为线程定义一个函数
+def print_time( threadName, delay):
+    count = 0
+    while count < 5:
+        time.sleep(delay)
+        count += 1
+        print ("%s: %s" % ( threadName, time.ctime(time.time()) ))
 
-print("tinydict['Age']: ", tinydict['Age'])
-print("tinydict['School']: ", tinydict['School'])
+# 创建两个线程
+try:
+    _thread.start_new_thread( print_time, ("Thread-1", 2, ) )
+    _thread.start_new_thread( print_time, ("Thread-2", 4, ) )
+except:
+    print ("Error: 无法启动线程")
+
+while 1:
+    pass
