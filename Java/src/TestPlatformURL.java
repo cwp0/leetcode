c41bd9e51325c7da2642d9172152678c606955f1
cwp0 <wenpengchen@njust.edu.cn>
2024-01-14 18:52:49 +0800

    feat: do some test.

--- Java/src/TestPlatformURL.java
+++ Java/src/TestPlatformURL.java
@@ -8,6 +8,9 @@
  * @Version: 1.0
  */
 public class TestPlatformURL {
+
+    public static String BASE_FILE_PATH = System.getProperty("os.name").toLowerCase() == "win" ? "D:\\gitkraken\\CWP\\platform\\FogSim\\modules\\cloudsim\\src\\main\\java\\org\\cloudbus\\fog\\" : "/Users/chenpengpeng/Documents/gitkraken/CWP/platform/FogSim/modules/cloudsim/src/main/java/org/cloudbus/fog/";
+
     public static void main(String[] args) {
         String os = System.getProperty("os.name").toLowerCase();
         System.out.println(os);
@@ -21,5 +24,6 @@ public class TestPlatformURL {
             // Handle other operating systems if needed
             System.out.println("other");
         }
+        System.out.println(BASE_FILE_PATH);
     }
 }
