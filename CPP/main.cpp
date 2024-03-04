b91214b21a27ce97ecf6526598e22e763284a9cd
cwp0 <wenpengchen@njust.edu.cn>
2024-03-04 19:48:39 +0800

    fix: update code.

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main() {
-    int r = 0;
-    start:
-    int m = r;
-    for(int i = m; i < 10; i++) {
-        cout << "前" << i << endl;
-        r = i + 2;
-        cout << r << endl;
-        cout << "后" << i << endl;
-        goto start;
-    }
 
+    int a = 123 % int(pow(10,2));
+
+    cout << pow(2,32) << endl;
     return 0;
 }
