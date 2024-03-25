ec8bbb49a54248383e10dfcfcdc708c02d10cb39
cwp0 <wenpengchen@njust.edu.cn>
2024-03-25 10:56:56 +0800

    q_300 done.

---
+++ Java/solutions/leetcode/editor/cn/[300]最长递增子序列.java
@@ -0,0 +1,76 @@
+// 
+// @Question: [300]最长递增子序列 
+// @Auther: cwp0
+// @CreatedTime: 2024-03-24 22:13:14
+// @Description: 
+//给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。 
+//
+// 子序列 是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子
+//序列。 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [10,9,2,5,3,7,101,18]
+//输出：4
+//解释：最长递增子序列是 [2,3,7,101]，因此长度为 4 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [0,1,0,3,2,3]
+//输出：4
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [7,7,7,7,7,7,7]
+//输出：1
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 2500 
+// -10⁴ <= nums[i] <= 10⁴ 
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 
+// 你能将算法的时间复杂度降低到 O(n log(n)) 吗? 
+// 
+//
+// Related Topics 数组 二分查找 动态规划 👍 3600 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        // 定义dp数组，记录以nums下标元素为结尾的最长递增子序列
+        int[] dp = new int[nums.length];
+        // base case，递增子序列至少要包括自己
+        Arrays.fill(dp, 1);
+
+        // 对某一元素，比较前面元素的数字和自己的数字，找到不超过自己数字的最长子序列，再加一
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j])
+                    // 注意这里是逐个跟所有小于dp[i]的子序列长度比较，所以有下面这一步
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+        }
+
+        int res = 0;
+        for (int i = 0; i < dp.length; i++) {
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
