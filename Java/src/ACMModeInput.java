bfbf68671e365d583c6f6ac7bb3e72cb310e513c
cwp0 <wenpengchen@njust.edu.cn>
2024-03-31 19:48:26 +0800

    fix: add ACM mode input format file.

---
+++ Java/src/ACMModeInput.java
@@ -0,0 +1,12 @@
+/**
+ * @Program: Java
+ * @Package: PACKAGE_NAME
+ * @Class: ACMModeInput
+ * @Description: ACM模式下做题的输入处理方式。
+ * @Author: cwp0
+ * @CreatedTime: 2024/03/31 19:47
+ * @Version: 1.0
+ */
+public class ACMModeInput {
+    
+}
