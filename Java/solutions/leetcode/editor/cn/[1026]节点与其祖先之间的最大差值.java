4de47074abd4b0aab2af8c0c1ff7e88cae014f90
cwp0 <wenpengchen@njust.edu.cn>
2024-04-05 15:05:09 +0800

    q_1026 done.

---
+++ Java/solutions/leetcode/editor/cn/[1026]节点与其祖先之间的最大差值.java
@@ -0,0 +1,84 @@
+// 
+// @Question: [1026]节点与其祖先之间的最大差值 
+// @Author: cwp0
+// @CreatedTime: 2024-04-05 13:53:12
+// @Description: 
+//给定二叉树的根节点 root，找出存在于 不同 节点 A 和 B 之间的最大值 V，其中 V = |A.val - B.val|，且 A 是 B 的祖先。 
+//
+//
+// （如果 A 的任何子节点之一为 B，或者 A 的任何子节点是 B 的祖先，那么我们认为 A 是 B 的祖先） 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：root = [8,3,10,1,6,null,14,null,null,4,7,13]
+//输出：7
+//解释： 
+//我们有大量的节点与其祖先的差值，其中一些如下：
+//|8 - 3| = 5
+//|3 - 7| = 4
+//|8 - 1| = 7
+//|10 - 13| = 3
+//在所有可能的差值中，最大值 7 由 |8 - 1| = 7 得出。
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：root = [1,null,2,null,0,3]
+//输出：3
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中的节点数在 2 到 5000 之间。 
+// 0 <= Node.val <= 10⁵ 
+// 
+//
+// Related Topics 树 深度优先搜索 二叉树 👍 246 👎 0
+
+// 时间复杂度：O()
+// 空间复杂度：O()
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxAncestorDiff(TreeNode root) {
+
+        return dfs(root, root.val, root.val);
+    }
+
+    private int dfs(TreeNode root, int max, int min) {
+        if (root == null)
+            return 0;
+
+        int diff = Math.max(Math.abs(root.val - max), Math.abs(root.val - min));
+        max = Math.max(root.val, max);
+        min = Math.min(root.val, min);
+
+        diff = Math.max(diff, dfs(root.left, max, min));
+        diff = Math.max(diff, dfs(root.right, max, min));
+        return diff;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
