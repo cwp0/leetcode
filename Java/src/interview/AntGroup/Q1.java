f1290554f4fb3c0c841360e06eab7b9e5c6a99c8
cwp0 <wenpengchen@njust.edu.cn>
2024-04-06 20:42:23 +0800

    update AntGroup questions.

---
+++ Java/src/interview/AntGroup/Q1.java
@@ -0,0 +1,15 @@
+package interview.AntGroup;
+
+/**
+ * @Program: Java
+ * @Package: interview.AntGroup
+ * @Class: Q1
+ * @Description:
+ * @Author: cwp0
+ * @CreatedTime: 2024/04/06 18:50
+ * @Version: 1.0
+ */
+public class Q1 {
+    // 小红在一个3*3的矩阵上，准备放置x个狗，y个猫，要求猫和猫之间，狗和狗之间不能相邻（上下左右都是相邻）。小红想知道，有多少种不同的放置方案？
+    // 0 <= x, y <= 9
+}
---
+++ Java/src/interview/AntGroup/Q2.java
@@ -0,0 +1,24 @@
+package interview.AntGroup;
+
+/**
+ * @Program: Java
+ * @Package: interview.AntGroup
+ * @Class: Q2
+ * @Description:
+ * @Author: cwp0
+ * @CreatedTime: 2024/04/06 20:41
+ * @Version: 1.0
+ */
+public class Q2 {
+    /*
+    小红正在学习 KMP 算法，KMP 算法是用于计算一个字符串的循环节的算法，例如 "abcabc" 的循环节就是 "abc" ， "aba" 的循环节是 "aba" 。
+
+    小红有一个字符串 s ，她想重排这个字符串，使得字符串循环节的长度最短，请输出重排后的字符串。
+
+    jAVA acm模式
+
+    输入cabbac
+    输出abcabc
+
+     */
+}
