640434278e3990ca4f1c49818cd84cfc681bb40e
cwp0 <wenpengchen@njust.edu.cn>
2024-04-11 16:01:59 +0800

    q_438 done.

---
+++ Java/solutions/leetcode/editor/cn/[438]找到字符串中所有字母异位词.java
@@ -0,0 +1,96 @@
+// 
+// @Question: [438]找到字符串中所有字母异位词 
+// @Author: cwp0
+// @CreatedTime: 2024-04-09 22:22:07
+// @Description: 
+//给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，返回这些子串的起始索引。不考虑答案输出的顺序。 
+//
+// 异位词 指由相同字母重排列形成的字符串（包括相同的字符串）。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: s = "cbaebabacd", p = "abc"
+//输出: [0,6]
+//解释:
+//起始索引等于 0 的子串是 "cba", 它是 "abc" 的异位词。
+//起始索引等于 6 的子串是 "bac", 它是 "abc" 的异位词。
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: s = "abab", p = "ab"
+//输出: [0,1,2]
+//解释:
+//起始索引等于 0 的子串是 "ab", 它是 "ab" 的异位词。
+//起始索引等于 1 的子串是 "ba", 它是 "ab" 的异位词。
+//起始索引等于 2 的子串是 "ab", 它是 "ab" 的异位词。
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 1 <= s.length, p.length <= 3 * 10⁴ 
+// s 和 p 仅包含小写字母 
+// 
+//
+// Related Topics 哈希表 字符串 滑动窗口 👍 1418 👎 0
+
+import java.util.*;
+
+// 时间复杂度：O()
+// 空间复杂度：O()
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> res = new ArrayList<>();
+        // 滑动窗口
+        HashMap<Character, Integer> window = new HashMap<>();
+        HashMap<Character, Integer> need = new HashMap<>();
+        for (char c : p.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+
+        int left = 0, right = 0;
+        int valid = 0;
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            // 增大窗口
+            right++;
+            // 窗口改变后对数据进行处理
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                if (need.get(c).equals(window.get(c))) {
+                    valid++;
+                }
+            }
+
+            // 是否达到缩小窗口的条件
+            // 这里的窗口长度与p一样长
+            while (right - left == p.length()) {
+                if (valid == need.size()) {
+                    res.add(left);
+                }
+
+                char d = s.charAt(left);
+                left++;
+                // 窗口改变后对数据进行处理
+                if (need.containsKey(d)) {
+                    if (need.get(d).equals(window.get(d))) {
+                        valid--;
+                    }
+                    window.put(d, window.get(d) - 1);
+                }
+
+            }
+
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
