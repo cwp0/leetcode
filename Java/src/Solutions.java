0ba95cfee564327be6e8b52e1d924a45655f35b6
cwp0 <wenpengchen@njust.edu.cn>
2024-06-05 21:07:43 +0800

    update code.

---
+++ Java/src/Solutions.java
@@ -0,0 +1,11 @@
+/**
+ * @Program: Java
+ * @Package: PACKAGE_NAME
+ * @Class: Solutions
+ * @Description:
+ * @Author: cwp0
+ * @CreatedTime: 2024/06/05 21:07
+ * @Version: 1.0
+ */
+public class Solutions {
+}
