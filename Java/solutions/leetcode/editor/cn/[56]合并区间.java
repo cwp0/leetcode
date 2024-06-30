ffacd12ac461cd8723d6c29f59b2db1ebeadbe1a
cwp0 <wenpengchen@njust.edu.cn>
2024-06-30 17:35:42 +0800

    q_52 done.

---
+++ Java/solutions/leetcode/editor/cn/[56]合并区间.java
@@ -0,0 +1,72 @@
+// 
+// @Question: [56]合并区间 
+// @Author: cwp0
+// @CreatedTime: 2024-06-29 21:11:38
+// @Description: 
+//以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返
+//回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+//输出：[[1,6],[8,10],[15,18]]
+//解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：intervals = [[1,4],[4,5]]
+//输出：[[1,5]]
+//解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= intervals.length <= 10⁴ 
+// intervals[i].length == 2 
+// 0 <= starti <= endi <= 10⁴ 
+// 
+//
+// Related Topics 数组 排序 👍 2355 👎 0
+
+import java.util.*;
+
+// 时间复杂度：O()
+// 空间复杂度：O()
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        int n = intervals.length;
+        // 按区间起点升序排列，终点降序排列
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[0] == b[0]) {
+                return b[1] - a[1];
+            }
+            return a[0] - b[0];
+        });
+        List<int[]> res = new ArrayList<>();
+        res.add(intervals[0]);
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+
+        for (int i = 1; i < n; i++) {
+            int[] intv = intervals[i];
+            int[] last = res.get(res.size() - 1);
+            // 重叠了
+            if (intv[0] <= last[1]) {
+                // 找到两个区间中最大的 end
+                last[1] = Math.max(last[1], intv[1]);
+            } else {
+                res.add(intv);
+            }
+        }
+        return res.toArray(new int[res.size()][]);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
