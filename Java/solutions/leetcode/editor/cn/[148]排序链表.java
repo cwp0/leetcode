704195347b97c6a49773054781f780dc38b3a2b6
cwp0 <wenpengchen@njust.edu.cn>
2024-08-08 15:29:11 +0800

    q_148 done.

---
+++ Java/solutions/leetcode/editor/cn/[148]排序链表.java
@@ -0,0 +1,80 @@
+// 
+// @Question: [148]排序链表 
+// @Author: cwp0
+// @CreatedTime: 2024-08-08 14:54:34
+// @Description: 
+//给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。 
+//
+// 
+// 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：head = [4,2,1,3]
+//输出：[1,2,3,4]
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：head = [-1,5,3,4,0]
+//输出：[-1,0,3,4,5]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目在范围 [0, 5 * 10⁴] 内 
+// -10⁵ <= Node.val <= 10⁵ 
+// 
+//
+// 
+//
+// 进阶：你可以在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？ 
+//
+// Related Topics 链表 双指针 分治 排序 归并排序 👍 2346 👎 0
+
+// 时间复杂度：O()
+// 空间复杂度：O()
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+import java.util.*;
+
+class Solution {
+    public ListNode sortList(ListNode head) {
+        List<Integer> nums = new ArrayList<>();
+        for (ListNode p = head; p != null; p = p.next) {
+            nums.add(p.val);
+        }
+        Collections.sort(nums);
+        ListNode p = head;
+        for (int num : nums) {
+            p.val = num;
+            p = p.next;
+        }
+        return head;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
