3cb66b466f4938f2139a91950ce2c4d32a353a53
cwp0 <wenpengchen@njust.edu.cn>
2024-08-14 16:29:02 +0800

    q_46 done.

---
+++ Java/solutions/leetcode/editor/cn/[46]全排列.java
@@ -0,0 +1,78 @@
+// 
+// @Question: [46]全排列 
+// @Author: cwp0
+// @CreatedTime: 2024-08-14 15:46:10
+// @Description: 
+//给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,2,3]
+//输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [0,1]
+//输出：[[0,1],[1,0]]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [1]
+//输出：[[1]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 6 
+// -10 <= nums[i] <= 10 
+// nums 中的所有整数 互不相同 
+// 
+//
+// Related Topics 数组 回溯 👍 2942 👎 0
+
+import java.util.*;
+
+// 时间复杂度：O()
+// 空间复杂度：O()
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    List<List<Integer>> res;
+    public List<List<Integer>> permute(int[] nums) {
+        res = new LinkedList<>();
+        LinkedList<Integer> track = new LinkedList<>(); // 记录「路径」
+        boolean[] used = new boolean[nums.length]; //「路径」中的元素会被标记为 true，避免重复使用
+        backtrack(nums, track, used);
+        return res;
+    }
+    private void backtrack(int[] nums, LinkedList<Integer> track, boolean[] used) {
+        if (track.size() == nums.length) {
+            res.add(new LinkedList<>(track));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) { // 排除不合法的选择
+                // nums[i] 已经在 track 中，跳过
+                continue;
+            }
+            // 做选择
+            track.add(nums[i]);
+            used[i] = true;
+            // 进入下一层决策树
+            backtrack(nums, track, used);
+            // 取消选择
+            track.removeLast();
+            used[i] = false;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
