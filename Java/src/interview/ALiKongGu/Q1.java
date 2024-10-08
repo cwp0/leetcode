adf7a3dcd2adec73232a6326fe4446945960fe4e
cwp0 <wenpengchen@njust.edu.cn>
2024-10-08 14:20:45 +0800

    add ALiKongGu interview questions.

---
+++ Java/src/interview/ALiKongGu/Q1.java
@@ -0,0 +1,17 @@
+package interview.ALiKongGu;
+
+/**
+ * @Program: Java
+ * @Package: interview.ALiKongGu
+ * @Class: Q1
+ * @Description:
+ * @Author: cwp0
+ * @CreatedTime: 2024/09/28 19:10
+ * @Version: 1.0
+ */
+import java.util.*;
+public class Q1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Java/src/interview/ALiKongGu/Q3.java
@@ -0,0 +1,13 @@
+package interview.ALiKongGu;
+
+/**
+ * @Program: Java
+ * @Package: interview.ALiKongGu
+ * @Class: Q3
+ * @Description:
+ * @Author: cwp0
+ * @CreatedTime: 2024/09/28 19:11
+ * @Version: 1.0
+ */
+public class Q3 {
+}
