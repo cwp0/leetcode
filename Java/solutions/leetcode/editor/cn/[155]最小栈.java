3602bebdf736472ebe96bcdee10f0ccbef231cf7
cwp0 <wenpengchen@njust.edu.cn>
2024-10-21 20:07:50 +0800

    q_155 done.

---
+++ Java/solutions/leetcode/editor/cn/[155]最小栈.java
@@ -0,0 +1,99 @@
+// 
+// @Question: [155]最小栈 
+// @Author: cwp0
+// @CreatedTime: 2024-10-21 17:37:39
+// @Description: 
+//设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。 
+//
+// 实现 MinStack 类: 
+//
+// 
+// MinStack() 初始化堆栈对象。 
+// void push(int val) 将元素val推入堆栈。 
+// void pop() 删除堆栈顶部的元素。 
+// int top() 获取堆栈顶部的元素。 
+// int getMin() 获取堆栈中的最小元素。 
+// 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入：
+//["MinStack","push","push","push","getMin","pop","top","getMin"]
+//[[],[-2],[0],[-3],[],[],[],[]]
+//
+//输出：
+//[null,null,null,null,-3,null,0,-2]
+//
+//解释：
+//MinStack minStack = new MinStack();
+//minStack.push(-2);
+//minStack.push(0);
+//minStack.push(-3);
+//minStack.getMin();   --> 返回 -3.
+//minStack.pop();
+//minStack.top();      --> 返回 0.
+//minStack.getMin();   --> 返回 -2.
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// -2³¹ <= val <= 2³¹ - 1 
+// pop、top 和 getMin 操作总是在 非空栈 上调用 
+// push, pop, top, and getMin最多被调用 3 * 10⁴ 次 
+// 
+//
+// Related Topics 栈 设计 👍 1830 👎 0
+
+// 时间复杂度：O()
+// 空间复杂度：O()
+//leetcode submit region begin(Prohibit modification and deletion)
+import java.util.*;
+class MinStack {
+    Stack<Integer> stack;
+    // 记录每次插入后的最小元素
+    Stack<Integer> minStack;
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val < minStack.peek()) {
+            // 存最小元素的栈是空的或者当前插入的值小于最小栈的栈顶元素
+            minStack.push(val);
+        } else {
+            // 当前插入的val大于最小栈的栈顶元素
+            minStack.push(minStack.peek());
+        }
+    }
+    
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+//leetcode submit region end(Prohibit modification and deletion)
